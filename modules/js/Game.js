9935a5c0e5d92eae0b2c9fde59689badd3a822b5
XIV <xivnick@gmail.com>
2026-04-03 12:19:29 +0900

    Use custom non-log message for inactive turn clicks

--- modules/js/Game.js
+++ modules/js/Game.js
@@ -1905,7 +1905,7 @@ export class Game {
 
     canInteract(action) {
         if (!this.isCurrentPlayerActive) {
-            this.bga.dialogs.showMoveUnauthorized();
+            this.bga.dialogs.showMessage(_('This is not your turn'), 'info');
             return false;
         }
         return this.bga.actions.checkAction(action, false);
