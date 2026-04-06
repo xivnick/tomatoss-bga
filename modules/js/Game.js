9047a95835d65aa7e9145406b06fc76d85f586c9
XIV <xivnick@gmail.com>
2026-04-07 02:22:53 +0900

    Increase discard popup card density

--- modules/js/Game.js
+++ modules/js/Game.js
@@ -1966,7 +1966,7 @@ export class Game {
             return;
         }
 
-        const scale = this.sprites.getCardScale('tomato') * 0.62;
+        const scale = this.sprites.getCardScale('tomato') * 0.82;
         cardsRoot.replaceChildren();
         cards.forEach((card, index) => {
             const cardHost = document.createElement('div');
